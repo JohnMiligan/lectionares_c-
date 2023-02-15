01d954d507739ae9028fcd0a2775ca0060b3d05b
Max Pilyuk <maximpil.ru@mail.ru>
2023-02-16 02:07:51 +0300

    added condition

--- lesson_1.4/Program.cs
+++ lesson_1.4/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿Console.Write("Введите имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша"){
+    Console.WriteLine("Ура, Маша!");
+}
+else{
+    Console.Write("Привет, " + username);
+}
