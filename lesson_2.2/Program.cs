d8b62d5d11e3b8817fd6feb4bb0ef32cb48559c6
Max Pilyuk <maximpil.ru@mail.ru>
2023-02-20 18:52:15 +0300

    lesson 2

---
+++ lesson_2.2/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 34, 35, 12, 454, 76, 23, 443, 6767, 23, 12 };
+
+int n = array.Length;
+int find = 23;
+int index = 0;
+
+while (index < n)
+{
+
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
